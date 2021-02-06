39602aa0163b69160305e83a3452f2542689f27e
Pheblar <75424398+Pheblar@users.noreply.github.com>
2021-02-06 20:27:15 +0300

    feat: cosmetic fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ exports.min = function min (array) {
   }
   let min = array[0];
   for (let i = 0; i < array.length; i++) {
-      if (min > array[i]) min = array[i];
+      if (min > array[i]) {min = array[i]};
   }
   return min;
 }
@@ -16,7 +16,7 @@ exports.max = function max (array) {
 }
   let max = array[0];
   for (let i = 0; i < array.length; i++) {
-      if (max < array[i]) max = array[i];
+      if (max < array[i]) {max = array[i]};
 }
   return max;
 }
